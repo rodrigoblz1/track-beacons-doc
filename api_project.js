2b3cc5dbb8963f6e559b260a0ed74112ec586b33
IFNET-FOR\rodrigo.bruno <rodrigo.bruno@ifactory.com.br>
2018-05-30 11:13:19 -0300

    Retirando atributo que não é mais utilizado

--- api_project.js
+++ api_project.js
@@ -21,7 +21,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-30T14:07:37.673Z",
+    "time": "2018-05-30T14:12:55.625Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
